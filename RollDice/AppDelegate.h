5f682775d07dba0989411df7bdef3298559caca5
Kapil Khatri <kapil@Kapils-Mac-mini.local>
2019-04-09 13:51:45 +0530

    Initial Commit

---
+++ RollDice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RollDice
+//
+//  Created by Kapil Khatri on 09/04/19.
+//  Copyright © 2019 Kapil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
